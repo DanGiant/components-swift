5e27a4221f4b5abe1b029e1052196d9e99417e57
james <james53171370@gmail.com>
2026-01-10 11:06:57 +0800

    switch to use GoCode-sg's client-sdk-swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/DanGiant/client-sdk-swift.git",
-                 branch: "develop"),
+        .package(url: "https://github.com/GoCode-sg/client-sdk-swift",
+                 branch: "v2.9.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.4.3"),
     ],
     targets: [
